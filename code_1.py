33fe139c7b59ad6227f4ac6ef1ea140ee7882c4d
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-11-24 13:16:23 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,38 @@
+        for (int item : data) {
+        }
+public class RandomDataGenerator {
+        }
+    public static void main(String[] args) {
+    }
+            System.out.println("Random Number: " + item);
+}
+
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
+            data[i] = (int) (Math.random() * 100) + 1;    }
+
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        }
+        for (int i = 0; i < 10; i++) {
+}
+        }
+        for (int item : data) {
+
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;def main():
+    data = generate_random_data()
+
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+import random
+def generate_random_data():
+
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+    for item in data:
